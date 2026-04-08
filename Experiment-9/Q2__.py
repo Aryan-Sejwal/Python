f05361224897dfb2f87391b4d4983dac27a6b168
Aryan-Sejwal <aryansejwal2020@gmail.com>
2026-04-09 00:50:40 +0530

    changes

--- Experiment-9/Q2__.py
+++ Experiment-9/Q2__.py
@@ -5,7 +5,6 @@ class Student:
         self.phy = phy
         self.chem = chem
         self.maths = maths
-
     def display(self):
         print("\nStudent Details")
         print("Name:", self.name)
@@ -13,21 +12,17 @@ class Student:
         print("Physics:", self.phy)
         print("Chemistry:", self.chem)
         print("Maths:", self.maths)
-
     def find_marks_percentage(self):
         total = self.phy + self.chem + self.maths
         percentage = total / 3
         return percentage
-
     def result(self):
         if self.phy > 40 and self.chem > 40 and self.maths > 40:
             print("Result: PASS")
         else:
             print("Result: FAIL")
-
 n = int(input("Enter number of students: "))
 students = []
-
 for i in range(n):
     print(f"\nEnter details of student {i+1}")
     name = input("Name: ")
@@ -35,20 +30,15 @@ for i in range(n):
     phy = int(input("Physics marks: "))
     chem = int(input("Chemistry marks: "))
     maths = int(input("Maths marks: "))
-
     s = Student(name, sap_id, phy, chem, maths)
     students.append(s)
-
 for s in students:
     s.display()
     print("Percentage:", s.find_marks_percentage())
     s.result()
-
 def class_average(students):
     total = 0
     for s in students:
         total += s.find_marks_percentage()
     return total / len(students)
-
-
 print("\nClass Average Percentage:", class_average(students))
