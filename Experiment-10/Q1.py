06079a7fceda82daacfb9930a95b3b21b8b1cd8f
Aryan-Sejwal <aryansejwal2020@gmail.com>
2026-04-11 14:35:12 +0530

    updates

--- Experiment-10/Q1.py
+++ Experiment-10/Q1.py
@@ -3,3 +3,8 @@ n = int(input("Enter number of elements: "))
 arr = np.array(list(map(int, input("Enter elements: ").split())))
 print("Array:", arr)
 print("Sum:", np.sum(arr))
+
+
+
+#import numpy as np
+#arr=np.array([10,20,30,40,50])
