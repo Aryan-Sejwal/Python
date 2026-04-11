0167d5ceaadffdc1231a990390660eafc6685fe1
Aryan-Sejwal <aryansejwal2020@gmail.com>
2026-04-11 14:42:00 +0530

    code

--- Experiment-10/Q2.py
+++ Experiment-10/Q2.py
@@ -1,8 +1,10 @@
 import numpy as np
-arr = np.array([[10,20,30],
-[40,50,60],
-[70,80,90]])
-print("Array:\n", arr)
-print("Row-wise sum:", arr.sum(axis=1))
-print("Column-wise sum:", arr.sum(axis=0))
-print("Second maximum:", np.sort(arr.flatten())[-2])
+arr = np.array([[1,2,3],[4,5,6],[7,8,9]])
+row_sum = np.sum(arr,axis=1)
+col_sum = np.sum(arr,axis=0)
+flat_arr = arr.flatten()
+sorted_arr = np.sort(flat_arr)
+second_max = sorted_arr[-2]
+print("Sum of rows: ",row_sum)
+print("Sum of columns: ",col_sum)
+print("Second maximum element is: ",second_max)
